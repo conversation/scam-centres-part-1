ca8b604d757b2bed851f7470ec13335db03c827f
Matt Garrow <mjkgarrow@gmail.com>
2025-01-23 13:17:08 +1100

    edit: tweaked pullquote styling

--- src/components/Pullquote.tsx
+++ src/components/Pullquote.tsx
@@ -12,16 +12,16 @@ export default function Pullquote({
   endQuoteIcon: boolean
 }) {
   return (
-    <div className={cn('text-balance py-5 text-center text-xl text-red-50 sm:text-2xl', className)}>
+    <div className={cn('text-balance py-5 text-center text-xl text-red-100 sm:text-2xl', className)}>
       <div className='px-0 sm:px-8'>
         <div
           className={cn(
             'relative inline-block',
             startQuoteIcon
-              ? 'before:absolute before:-top-2 before:left-0 before:text-[10rem] before:leading-none before:text-white before:opacity-10 before:content-["“"]'
+              ? 'before:absolute before:-top-2 before:left-0 before:text-[10rem] before:leading-none before:text-white before:opacity-20 before:content-["“"]'
               : '',
             endQuoteIcon
-              ? 'after:absolute after:right-0 after:top-[80%] after:text-[10rem] after:leading-none after:text-white after:opacity-10 after:content-["”"]'
+              ? 'after:absolute after:right-0 after:top-[80%] after:text-[10rem] after:leading-none after:text-white after:opacity-20 after:content-["”"]'
               : ''
           )}
         >
