ce0493d07162091fd725fa8226f3d89c66becf59
Matt Garrow <mjkgarrow@gmail.com>
2025-01-23 13:08:42 +1100

    feat: added tweet embed element

---
+++ src/components/Tweet1.tsx
@@ -0,0 +1,17 @@
+export default function Tweet1() {
+  return (
+    <div className='flex w-[min(100%,750px)] justify-center'>
+      <blockquote className='twitter-tweet'>
+        <p lang='en' dir='ltr'>
+          A Chinese actor reported missing at the Thailand-Myanmar border has been found after fears that he might have
+          entered a Myanmese city known as an online scam hub. Read more:{' '}
+          <a href='https://t.co/sSxZVOZG3R'>https://t.co/sSxZVOZG3R</a>{' '}
+          <a href='https://t.co/WZFsVU0kcR'>pic.twitter.com/WZFsVU0kcR</a>
+        </p>
+        &mdash; South China Morning Post (@SCMPNews){' '}
+        <a href='https://twitter.com/SCMPNews/status/1876946965793300485?ref_src=twsrc%5Etfw'>January 8, 2025</a>
+      </blockquote>{' '}
+      <script async src='https://platform.twitter.com/widgets.js'></script>
+    </div>
+  )
+}
