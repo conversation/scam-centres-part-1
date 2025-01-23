fa1a6f0c2d72264d9acba2d1e409ac26d0d4115b
Matt Garrow <mjkgarrow@gmail.com>
2025-01-23 14:18:21 +1100

    edit: changed to AUD conversion

--- src/components/SeverancePaymentBill.tsx
+++ src/components/SeverancePaymentBill.tsx
@@ -83,23 +83,23 @@ export default function SeverancePaymentBill() {
       <div className='not-prose max-md:w-image-width w-image-width-md absolute left-1/2 top-[-0.6%] aspect-[1075/1263] -translate-x-1/2 max-sm:left-[55%] sm:aspect-[103/121]'>
         <div className='absolute right-[12%] top-[42.7%] rotate-[6.1deg]'>
           <span className='scramble1 bg-neutral-900 p-1 opacity-0'>Floor Damage Fee</span>
-          <span className='scramble1 ml-6 bg-neutral-900 p-1 opacity-0 md:ml-10'>US$1,179.33</span>
+          <span className='scramble1 ml-6 bg-neutral-900 p-1 opacity-0 md:ml-10'>~A$1,750</span>
         </div>
         <div className='absolute right-[13%] top-[46.3%] rotate-[6.1deg]'>
           <span className='scramble2 bg-neutral-900 p-1 opacity-0'>Chair Damage Fee</span>
-          <span className='scramble2 ml-6 bg-neutral-900 p-1 opacity-0 md:ml-10'>US$589.67</span>
+          <span className='scramble2 ml-6 bg-neutral-900 p-1 opacity-0 md:ml-10'>~A$875</span>
         </div>
         <div className='absolute right-[14%] top-[57.1%] rotate-[6.8deg]'>
           <span className='scramble2 bg-neutral-900 p-1 opacity-0'>Drinking Water</span>
-          <span className='scramble2 ml-6 bg-neutral-900 p-1 opacity-0 md:ml-10'>US$442.25</span>
+          <span className='scramble2 ml-6 bg-neutral-900 p-1 opacity-0 md:ml-10'>~A$650</span>
         </div>
         <div className='absolute right-[15.6%] top-[67.7%] rotate-[7.3deg]'>
           <span className='scramble3 bg-neutral-900 p-1 opacity-0'>Toilet Usage Fee</span>
-          <span className='scramble3 ml-6 bg-neutral-900 p-1 opacity-0 md:ml-10'>US$324.32</span>
+          <span className='scramble3 ml-6 bg-neutral-900 p-1 opacity-0 md:ml-10'>~A$480</span>
         </div>
         <div className='absolute right-[16%] top-[70.5%] rotate-[7.6deg]'>
           <span className='scramble4 bg-neutral-900 p-1 opacity-0'>Seaside Air Breathing Fee</span>
-          <span className='scramble4 ml-6 bg-neutral-900 p-1 opacity-0 md:ml-10'>US$1,076.14</span>
+          <span className='scramble4 ml-6 bg-neutral-900 p-1 opacity-0 md:ml-10'>~A$1,600</span>
         </div>
       </div>
     </section>
