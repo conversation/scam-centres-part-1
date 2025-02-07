7cc0eff79a69cf0e95296c54d51c740dca416777
Matt Garrow <mjkgarrow@gmail.com>
2025-02-07 18:00:24 +1100

    edit: cleaned up message component

--- src/components/TextMessage.tsx
+++ src/components/TextMessage.tsx
@@ -1,61 +1,63 @@
 import Imgix from 'react-imgix'
 import { cn } from '../util/helpers'
-import { ReactNode } from 'react'
+import { forwardRef, ReactNode } from 'react'
 
-export default function TextMessage({
-  children,
-  className,
-  sender,
-  time
-}: {
+type TextMessageProps = {
   children: ReactNode
-  className: string
-  sender: true | boolean
+  className?: string
+  sender: boolean
   time: string
-}) {
-  return (
-    <>
-      {sender ? (
-        <div
-          className={cn(
-            'message_bubble not-prose message_bubble_right rounded-md rounded-tr-none p-2 pr-3 text-[#e2ffc7]',
-            className
-          )}
-        >
-          <div className='text-black'>{children}</div>
-          <div className='float-right flex items-end gap-1 text-xs text-neutral-400'>
-            <p>{time}</p>
+} & React.HTMLProps<HTMLDivElement>
 
-            <Imgix
-              className='inline aspect-square max-h-5 w-5 max-w-5'
-              imgixParams={{
-                fit: 'crop',
-                auto: 'format',
-                q: 30
-              }}
-              htmlAttributes={{
-                loading: 'lazy',
-                width: '100%',
-                height: 'auto'
-              }}
-              src='https://images.theconversation.com/files/617201/original/file-20240904-16-xvj31z.png'
-              sizes='(max-width: 599px) 100px, (min-width: 600px) 100px, 100px'
-            />
+export const TextMessage = forwardRef<HTMLDivElement, TextMessageProps>(
+  ({ children, className, sender, time, ...rest }, ref) => {
+    return (
+      <>
+        {sender ? (
+          <div
+            ref={ref}
+            className={cn(
+              'message_bubble not-prose message_bubble_right rounded-md rounded-tr-none p-2 pr-3 text-[#e2ffc7]',
+              className
+            )}
+            {...rest}
+          >
+            <div className='text-black'>{children}</div>
+            <div className='flex items-end justify-end gap-1 text-xs text-neutral-400'>
+              <p>{time}</p>
+              <Imgix
+                className='inline aspect-square max-h-5 w-5 max-w-5'
+                imgixParams={{
+                  fit: 'crop',
+                  auto: 'format',
+                  q: 30
+                }}
+                htmlAttributes={{
+                  loading: 'lazy',
+                  width: '100%',
+                  height: 'auto'
+                }}
+                src='https://images.theconversation.com/files/617201/original/file-20240904-16-xvj31z.png'
+                sizes='(max-width: 599px) 100px, (min-width: 600px) 100px, 100px'
+              />
+            </div>
           </div>
-        </div>
-      ) : (
-        <div
-          className={cn(
-            'message_bubble not-prose message_bubble_left rounded-md rounded-tl-none p-2 pl-3 text-neutral-50',
-            className
-          )}
-        >
-          <div className='text-black'>{children}</div>
-          <div className='float-right flex items-end gap-1 text-xs text-neutral-400'>
-            <p>{time}</p>
+        ) : (
+          <div
+            ref={ref}
+            className={cn(
+              'message_bubble not-prose message_bubble_left rounded-md rounded-tl-none p-2 pl-3 text-neutral-50',
+              className
+            )}
+            {...rest}
+          >
+            <div className='text-black'>{children}</div>
+            <div className='flex items-end justify-end gap-1 text-xs text-neutral-400'>
+              <p>{time}</p>
+            </div>
           </div>
-        </div>
-      )}
-    </>
-  )
-}
+        )}
+      </>
+    )
+  }
+)
