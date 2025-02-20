d762774378d36e93ee05d89bd171a3d49272e893
Matt Garrow <mjkgarrow@gmail.com>
2025-02-20 15:26:21 +1100

    edit: readmore text changes

--- src/components/Readmore.tsx
+++ src/components/Readmore.tsx
@@ -1,42 +1,25 @@
-export default function Readmore({
-  url,
-  description,
-  linkText,
-  iconSrc
-}: {
-  url: string
-  description: string
-  linkText: string
-  iconSrc?: string
-}) {
+export default function Readmore() {
   return (
-    <>
-      {iconSrc ? (
-        <div className='relative my-10 grid grid-cols-[minmax(5rem,10%)_1fr] items-center gap-4 border-y border-neutral-500 py-4'>
-          <div className='not-prose aspect-square w-full overflow-hidden rounded-md'>
-            <img src={iconSrc} alt='' className='not_fullscreen_media' />
-          </div>
-          <p className='!mb-0 !mt-0 italic'>
-            <span className=''>
-              Readmore: {description}
-              <a href={url} target='_parent' className='text-neutral-200'>
-                {linkText}
-              </a>
-            </span>
-          </p>
-        </div>
-      ) : (
-        <div className='relative my-10 grid items-center gap-4 border-y border-neutral-500 py-4'>
-          <p className='!mb-0 !mt-0 italic'>
-            <span className=''>
-              Readmore: {description}
-              <a href={url} target='_parent' className='text-neutral-200'>
-                {linkText}
-              </a>
-            </span>
-          </p>
-        </div>
-      )}
-    </>
+    <div className='relative my-10 grid grid-cols-[minmax(5rem,10%)_1fr] gap-4 border-y border-neutral-500 py-4 sm:grid-cols-[minmax(8rem,10%)_1fr]'>
+      <div className='not-prose aspect-square w-full overflow-hidden rounded-md'>
+        <a href='#' target='_parent' className='text-neutral-100 hover:text-neutral-200'>
+          <img
+            src='https://images.theconversation.com/files/645965/original/file-20250130-15-bubzm1.png?ixlib=rb-4.1.0&q=20&auto=format&w=128'
+            alt=''
+            className='not_fullscreen_media h-32 w-32 max-sm:h-20 max-sm:w-20'
+          />
+        </a>
+      </div>
+      <p className='!mb-0 !mt-0 italic'>
+        <a href='#' target='_parent' className='text-neutral-100 hover:text-neutral-200'>
+          Scam Factories
+        </a>{' '}
+        is a special multimedia and podcast series by The Conversation. Listen to the{' '}
+        <a href='#' target='_parent' className='text-neutral-100 hover:text-neutral-200'>
+          podcast series here
+        </a>
+        .
+      </p>
+    </div>
   )
 }
