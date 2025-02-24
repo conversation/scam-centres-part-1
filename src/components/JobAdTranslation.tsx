70a4aee6a35b086df94951bb595c12af1a870eb5
Matt Garrow <mjkgarrow@gmail.com>
2025-02-24 11:55:05 +1100

    edit: text changes

--- src/components/JobAdTranslation.tsx
+++ src/components/JobAdTranslation.tsx
@@ -70,53 +70,41 @@ export default function JobAdTranslation() {
 
       <div className='sm:imgfloat-right not-prose absolute right-1/2 top-0 aspect-[95/166] h-screen max-h-[600px] w-auto translate-x-1/2 sm:right-0 sm:h-auto sm:w-full sm:translate-x-0'>
         <div className='absolute left-[9%] top-[10%]'>
-          <span className='translation bg-neutral-900 p-1 opacity-0'>Position: General worker; Finance; Business</span>
-        </div>
-        <div className='absolute left-[9%] top-[16%]'>
-          <span className='translation bg-neutral-900 p-1 opacity-0'>Functions:</span>
-        </div>
-        <div className='absolute left-[9%] top-[20%]'>
-          <span className='translation bg-neutral-900 p-1 opacity-0'>
+          <span className='translation bg-neutral-900 py-[0.1rem] opacity-0'>
+            Position: General worker, finance, business
+            <br />
+            <br />
+          </span>
+          <span className='translation bg-neutral-900 py-[0.1rem] opacity-0'>
+            Functions:
+            <br />
             1: Work within park and follow up on project construction
             <br />
             2: Information management, file sorting, collection, registration
           </span>
         </div>
-        {/* <div className='absolute left-[9%] top-[23%]'>
-          <span className='translation bg-neutral-900 p-1 opacity-0'>
-            2: Information management, file sorting, collection, registration
+
+        <div className='absolute left-[9%] top-[43%] w-[90%]'>
+          <span className='translation bg-neutral-900 py-[0.1rem] opacity-0'>
+            Benefits
+            <br />
           </span>
-        </div> */}
-        <div className='absolute left-[9%] top-[43%]'>
-          <span className='translation bg-neutral-900 p-1 opacity-0'>Benefits</span>
-        </div>
-        <div className='absolute left-[9%] top-[47%]'>
-          <span className='translation bg-neutral-900 p-1 opacity-0'>Salary: workshop position ¥19,000/month</span>
-        </div>
-        <div className='absolute left-[9%] top-[52.7%]'>
-          <span className='translation bg-neutral-900 p-1 opacity-0'>
+          <span className='translation bg-neutral-900 py-[0.1rem] opacity-0'>
+            Salary: workshop position ¥19,000 a month <br />
+          </span>
+          <span className='translation bg-neutral-900 py-[0.1rem] opacity-0'>
             1: Food and accommodation included
             <br />
             2: Standard working system, 8 working hours per day, 5 days of work and 2 days of rest
-          </span>
-        </div>
-        {/* <div className='absolute left-[9%] top-[56%]'>
-          <span className='translation bg-neutral-900 p-1 opacity-0'>
-            2: Standard working system, 8 working hours per day, 5 days of work and 2 days of rest
-          </span>
-        </div> */}
-        <div className='absolute left-[9%] top-[73%]'>
-          <span className='translation bg-neutral-900 p-1 opacity-0'>
+            <br />
+            3: Government authorisation... The company signs a labor contract with the employees [for] local social
+            security, medical insurance and other benefits
+            <br />
             4: Equipped with staff dormitory building
             <br />
             5: Stable work income, focus on employee benefits and needs
           </span>
         </div>
-        {/* <div className='absolute left-[9%] top-[77%]'>
-          <span className='translation bg-neutral-900 p-1 opacity-0'>
-            5: Stable work income, focus on employee benefits and needs
-          </span>
-        </div> */}
       </div>
     </section>
   )
